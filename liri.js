bea5319c28972e3664eadd0a397e0f4913cd38e0
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-10-12 23:22:09 -0700

    Added concert-this argument case

--- liri.js
+++ liri.js
@@ -1,4 +1,31 @@
 require("dotenv").config()
+const Spotify = require('node-spotify-api')
+const keys = require("./keys.js")
+const axios = require('axios')
+const fs = require('fs')
+const moment = require('moment')
 
-var keys = require("./keys.js")
-var spotify = new Spotify(keys.spotify)
+let spotify = new Spotify(keys.spotify)
+
+if (process.argv[2]) {
+  switch (process.argv[2]) {
+    case 'concert-this':
+      axios.get('https://rest.bandsintown.com/artists/maroon+5/events?app_id=codingbootcamp')
+        .then(r => {
+          r.data.forEach(x => {
+            console.log(`Venue Name: ${x.venue.name}`)
+            console.log(`Venue Location: ${x.venue.city}, ${x.venue.region}, ${x.venue.country}`)
+            console.log(`Event Date: ${moment(x.datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY')}`)
+          })
+        })
+        .catch(e => console.error(e))
+        .finally(_ => {
+          console.log('finally')
+        })
+      break
+    default:
+      break
+  }
+} else {
+  console.log('I need to know what to do.')
+}
