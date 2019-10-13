67f865f5f947b493ef0eb6e6b4b3a6a0cc02cf90
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-10-13 00:24:32 -0700

    Added  spotify-this-song argument case

--- liri.js
+++ liri.js
@@ -10,18 +10,36 @@ let spotify = new Spotify(keys.spotify)
 if (process.argv[2]) {
   switch (process.argv[2]) {
     case 'concert-this':
-      axios.get('https://rest.bandsintown.com/artists/maroon+5/events?app_id=codingbootcamp')
+      if (!process.argv[3]) {
+        console.log('Need a band to search for!')
+        break
+      }
+      axios.get(`https://rest.bandsintown.com/artists/${process.argv[3]}/events?app_id=codingbootcamp`)
         .then(r => {
           r.data.forEach(x => {
             console.log(`Venue Name: ${x.venue.name}`)
-            console.log(`Venue Location: ${x.venue.city}, ${x.venue.region}, ${x.venue.country}`)
+            console.log(`Venue Location: ${x.venue.city}, ${x.venue.region ? (x.venue.region+', ') : ''}${x.venue.country}`)
             console.log(`Event Date: ${moment(x.datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY')}`)
           })
         })
         .catch(e => console.error(e))
-        .finally(_ => {
-          console.log('finally')
+        .finally(_ => {})
+      break
+    case 'spotify-this-song':
+      spotify.search({ type: 'track', query: process.argv[3] ? process.argv[3] : 'The Sign Ace of Base', limit: 1 })
+        .then(r => {
+          const song = r.tracks.items[0]
+          let artists = ''
+          song.artists.forEach(artist => {
+            artists += artist.name + ', '
+          })
+          artists = artists.slice(0, artists.length - 2)
+          console.log(`Artist(s): ${artists}`)
+          console.log(`Name: ${song.name}`)
+          console.log(`Preview: ${song.preview_url}`)
+          console.log(`Album: ${song.album.name}`)
         })
+        .catch(e => console.error(e))
       break
     default:
       break
